eac871edd6575eabe0b9bc2c5ecb51bba75dc4c9
Isaac Soler <Isaac.i.soler@gmail.com>
2025-10-24 15:26:43 -0400

    Adding Assignment2

---
+++ Assignment2/index.js
@@ -0,0 +1,48 @@
+function reverseString(str) 
+{
+    return str.split('').reverse().join('');
+}
+
+function palindrome(str)
+{
+    const stringReversal = str.split('').reverse().join('');
+
+    if (str.toLowerCase() === stringReversal.toLowerCase())
+    {
+        return "This is a palindrome";
+    }
+    else
+    {
+        return "This is not a palindrome";
+    }
+
+}
+
+function totalCalculator(subtotal, tip)
+{
+    const total = subtotal + (subtotal * tip);
+    return total.toFixed(2);
+}
+
+document.getElementById("reverseButton").addEventListener("click", function() 
+{
+    const input = document.getElementById("reverseInput").value;
+    document.getElementById("reverseOutput").textContent = reverseString(input);
+});
+
+document.getElementById("palindromeButton").addEventListener("click", function() 
+{
+    const input = document.getElementById("palindromeInput").value;
+    document.getElementById("palindromeOutput").textContent = palindrome(input);
+});
+
+document.getElementById("tipButton").addEventListener("click", function() 
+{
+    const billAmount = parseFloat(document.getElementById("billAmount").value);
+    const tipPercentage = parseFloat(document.getElementById("tipPercentage").value) / 100;
+    document.getElementById("tipOutput").textContent = totalCalculator(billAmount, tipPercentage);
+
+//Didnt use parseFloat becuase it created more clutter and had less functionality
+//Also used conventional java script function syntax for better readability(I learned this in my AP CS class in highschool, shoutout Mr.Cangelosi)
+
+});
